dc4daebfb3e1c4b538af7c5c1dffd4e04be414d4
Javier Rodriguez <franciscoromera0816@gmail.com>
2025-10-07 20:48:12 +0200

    CalculaEdad en c# basico

---
+++ CalculaEdad/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Saludo y peticion de datos
+        Console.WriteLine("¡Hola! ¿Cómo te llamas?");
+        string nombre = Console.ReadLine();
+
+        Console.WriteLine("¿En qué Año naciste?");
+        string input = Console.ReadLine();
+
+        // Convertir la entrada a numero
+        int anioNacimiento = int.Parse(input);
+
+        // Calcula la edad
+        int anioActual = DateTime.Now.Year;
+        int edad = anioActual - anioNacimiento;
+
+        Console.WriteLine($"\nEncantado, {nombre}. Tienes aproximadamente {edad} años.");
+    }
+}
